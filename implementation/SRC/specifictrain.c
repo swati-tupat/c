f623eddcdfc6169025f7928bd21b6bdff4eaa20b
swati-tupat <56061805+swati-tupat@users.noreply.github.com>
2021-04-16 12:00:40 +0530

    Update specifictrain.c

--- implementation/SRC/specifictrain.c
+++ implementation/SRC/specifictrain.c
@@ -33,24 +33,24 @@ void specifictrain(int train_num)
 	{
 		printf("\nTrain:\t\t\tIron City Express");
 		printf("\nDestination:\t\tBoston to Chicago");
-		printf("\nDeparture:\t\t7am");
+		printf("\nDeparture:\t\t7:00 am");
 	}
 	if (train_num==1006)
 	{
 		printf("\ntrain:\t\t\tIron City Express");
 		printf("\nDestination:\t\tChicago to Boston");
-		printf("\nDeparture:\t\t9.30am ");
+		printf("\nDeparture:\t\t9.30 am ");
 	}
 	if (train_num==1007)
 	{
 		printf("\ntrain:\t\t\tKeystone Express");
 		printf("\nDestination:\t\tBoston to Washington");
-		printf("\nDeparture:\t\t1pm ");
+		printf("\nDeparture:\t\t1:00 pm ");
 	}
 	if (train_num==1008)
 	{
 		printf("\ntrain:\t\t\tKeystone Express");
 		printf("\n Destination:\t\tWashington to Boston");
-		printf("\nDeparture:\t\t4pm ");
+		printf("\nDeparture:\t\t4:00 pm ");
 	}
 }
