f3783a8fd0f5f6e17ba9d59777eeabf20d4df09a
swati-tupat <56061805+swati-tupat@users.noreply.github.com>
2021-04-16 12:12:07 +0530

    updated ..done

--- implementation/SRC/reservation.c
+++ implementation/SRC/reservation.c
@@ -17,17 +17,17 @@ void reservation(void)
 	FILE *fp;
 	fp=fopen("seats_reserved.txt","a");
 	printf("\e[1;1H\e[2J");
-	printf("\nEnter Your Name:> ");
+	printf("\nEnter Your Name: ");
 	fflush(stdin);
 	scanf("%s",passdetails.name);
 	printf("\nEnter Number of seats:> ");
 	scanf("%d",&passdetails.num_of_seats);
-	printf("\n\n>>Press Enter To View Available Trains<< ");
+	printf("\n\nPress Enter For Available Trains.. ");
 	getchar();
 	getchar();
 	printf("\e[1;1H\e[2J");
 	viewdetails();
-	printf("\n\nEnter train number:> ");
+	printf("\n\nEnter train number: ");
 	start1:
 	scanf("%d",&passdetails.train_num);
 	if(passdetails.train_num>=1001 && passdetails.train_num<=1010)
@@ -37,11 +37,11 @@ void reservation(void)
 	}
 	else
 	{
-		printf("\nInvalid train Number! Enter again--> ");
+		printf("\nInvalid train Number! Enter again : ");
 		goto start1;
 	}
 	
-	printf("\n\nConfirm Ticket (y/n):>");
+	printf("\n\nConfirm Ticket (yes/no):>");
 	start:
 	scanf(" %c",&confirm);
 	UNITY_BEGIN();
@@ -50,21 +50,22 @@ void reservation(void)
 	if(confirm == 'y')
 	{
 		fprintf(fp,"%s\t\t%d\t\t%d\t\t%.2f\n",passdetails.name,passdetails.num_of_seats,passdetails.train_num,charges);
-		printf("==================");
-		printf("\n Reservation Done\n");
-		printf("==================");
-		printf("\nPress any key to go back to Main menu");
+		printf("=========================================");
+		printf("\n TICKET BOOKING IS DONE \n");
+		printf("==========================================");
+		printf("\nPress any key to go back to Main menu_");
 		getchar();
 	}
 	else
 	{
 		if(confirm=='n'){
-			printf("\nReservation Not Done!\nPress any key to go back to  Main menu!");
+			printf("\nTICKET BOOKING IS FAILED");
+			printf("\nPress any key to go back to  Main menu_");
 			getchar();
 		}
 		else
 		{
-			printf("\nInvalid choice entered! Enter again-----> ");
+			printf("\nInvalid choice entered! Enter your choice again : ");
 			goto start;
 		}
 	}
